b7210a3da04a93c793dd8b813ff2c8cdcce55fa2
cassianms <ni.roblesc@duocuc.cl>
2026-04-13 09:51:54 -0400

    hola

---
+++ src/test/java/com/duoc/robloxmusic/RobloxmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.robloxmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobloxmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
